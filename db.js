f592f382c94759746a1df2994a24fa45756400f7
Diego Cardoso <cardo.tw@gmail.com>
2018-05-14 10:05:50 -0500

    dbjaw

--- db.js
+++ db.js
@@ -5,6 +5,13 @@ exports.connect = function(done)
   {
       con = mysql.createConnection(process.env.JAWSDB_URL)
       done()
+      con.connect();
+
+    con.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+      if (err) throw err;
+
+      console.log('The solution is: ', rows[0].solution);
+    });
    }
 
  exports.get = function()
