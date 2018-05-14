e34593c51b85366d811a7bbae6f39c53adac2b61
Diego Cardoso <cardo.tw@gmail.com>
2018-05-14 04:43:30 -0500

    nada

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
