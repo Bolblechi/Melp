2836bd7a33fcafca71f4da0cee4bf531f32d38e0
Diego Cardoso <cardo.tw@gmail.com>
2018-05-14 09:02:59 -0500

    listen

--- app.js
+++ app.js
@@ -27,11 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
